5ea89adf0378088e854a1f3a29b5f1b454ff27e3
johnabednego <jilimajohnabednego@gmail.com>
2024-05-16 16:27:31 +0000

    script

---
+++ Migrations/SQLScripts/initial_script.sql
@@ -0,0 +1,46 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Customers] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [ContactInformation] nvarchar(max) NOT NULL,
+    [CurrentBalance] decimal(18,2) NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Transactions] (
+    [Id] int NOT NULL IDENTITY,
+    [Date] datetime2 NOT NULL,
+    [UniqueNumber] nvarchar(max) NOT NULL,
+    [Remarks] nvarchar(max) NOT NULL,
+    [Type] int NOT NULL,
+    [Amount] decimal(18,2) NOT NULL,
+    [CustomerId] int NOT NULL,
+    CONSTRAINT [PK_Transactions] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Transactions_Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [Customers] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Transactions_CustomerId] ON [Transactions] ([CustomerId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240513130711_InitialCreate', N'8.0.5');
+GO
+
+COMMIT;
+GO
+
